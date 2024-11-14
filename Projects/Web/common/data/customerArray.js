5840ffd6e2a59de3e8df8ba6b48a3be5e43c394c
doduyquy <doduyquy211@gmail.com>
2024-11-14 11:12:29 +0700

    Unrar, add all file user, module, link customerArray to TESTjavascript, test oke DangKy, DangNhap

--- Projects/Web/common/data/customerArray.js
+++ Projects/Web/common/data/customerArray.js
@@ -6,7 +6,7 @@
  *  -
  *  -
  */
-export class Address {
+class Address {
     city = undefined;           // Thành phố
     district = undefined;       // Quận *
     ward = undefined;           // Phường
@@ -18,26 +18,27 @@ export class Address {
         return this.district;
     }
 }
-export class Customer {
-    username = undefined;   // (Work like ID), unique
-    password = undefined;   
+class Customer {
+    username = '';   // (Work like ID), unique
+    password = '';   
     
     /* Information của khách hàng: 
     có thể gộp thành Object nhưng để đơn giản -> không gộp */
-    phone = undefined;      
-    email = undefined;
+    phone = '';      
+    email = '';
     address = undefined;        // Object 
     
     /** status: tình trạng của khách hàng, bị lock hay chưa
      * 0: unavailable (đã bị lock)
      * 1: available (chưa lock, đang hoạt động)
      */
-    status = undefined;     // Default: 1 
+    status = 1;     // Default: 1 
     
-    constructor(username, password, phone, address, status = 1){
+    constructor(username, password, phone, email, address, status = 1){
         this.username = username;
         this.password = password;
         this.phone = phone;
+        this.email = email;
         this.address = address;
         this.status = 1;
     }
@@ -58,12 +59,55 @@ export class Customer {
 /* MẢNG KHÁCH HÀNG */
 // Load mảng lên từ localStorage
 export let customerArray = JSON.parse(localStorage.getItem('customerArray'));
+// console.log(customerArray);
 // Nếu customerArray chưa được tạo:
 if(!customerArray){
     customerArray = [];
+    console.log(customerArray);
+    
 }
 
 // Lưu mảng vào localStorage:
-function saveOrderArrayToStorage(){
-    localStorage.setItem('customerArray', JSON.stringify(customerArray))
+export function saveCustomerArrayToStorage(){
+    localStorage.setItem('customerArray', JSON.stringify(customerArray));
+    console.log('Saved customerArray to localStorage');
+}
+
+/** ----- USER ----- */
+// Kiểm tra trong mảng customer đã có tồn tại username ?
+// Có: return true
+// Không: return false
+export function checkExistedUsername(username) {
+    // !!! forEach: duyệt từ đầu đến cuối mảng không dừng với return.
+    // some: dừng lại vòng lặp ngay khi tìm thấy username tương ứng.
+    return customerArray.some((customer) => {
+        if (username === customer.username) {
+
+            console.log('Exised: pass user: ' + username);
+            return true; // Đã tồn tại
+        }
+        return false;
+    });
+}
+
+// Kiểm tra đăng nhập đúng:
+// false: username không tồn tại hoặc mật khẩu không đúng.
+// true: hợp lệ
+export function checkValidAccount(username, password){
+    const matchingCustomer = customerArray.find(customer => customer.username === username);
+    if(!matchingCustomer || matchingCustomer.password !== password){
+        return false;
+    }    
+    return true;
+}
+
+// Thêm new customer vào Array
+// Chỉ cần truyền username, password (khi customer mới đăng kí)
+export function addCustomerToArray(username, password, phone = '', email = '', address = '', status = 1){
+    const customer = new Customer(username, password);
+    customerArray.push(customer);
+    console.log('Added customer to customerArray');
+
+    // Lưu thay đổi vào localStorage:
+    saveCustomerArrayToStorage();
 }
